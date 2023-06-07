13e4854cf76f175f8c33d2999cb23a67574aad12
gamzeakyol06 <gamzeakyol06@gmail.com>
2023-06-07 13:29:29 +0300

    Merge remote-tracking branch 'github/main'

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -29,7 +29,7 @@ public class Base {
         chromeOptions.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(chromeOptions);
-        System.setProperty("webdriver.chrome.driver","/src/main/driver/chromedriver_linux64/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","PamisDashboardTest/ws/src/main/driver/chromedriver_linux64/chromedriver.exe");
         System.out.println(System.getProperty("webdriver.chrome.driver"));
 
         driver.manage().deleteAllCookies();
